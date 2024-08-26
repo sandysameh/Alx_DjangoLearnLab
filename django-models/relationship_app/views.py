b73f950aa2c6982153fa27c70e6f9eee5112c2db
sandysameh2 <sandy.sameh@nodogoro.com>
2024-08-26 17:28:23 +0300

    trial 5

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import Book, Library
+from .models import  Library
+from .models import  Book
+
 from django.views.generic.detail import DetailView
 
 
