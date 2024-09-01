fb6b0960f334e7d0bb7efa80e82db7309c7c0450
sandysameh2 <sandy.sameh@nodogoro.com>
2024-09-01 17:22:38 +0300

    books adjust

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
 
     path('register/',views.register,name="register"),
 
-        path('book/add/', views.add_book, name='add_book'),
-    path('book/edit/<int:pk>/', views.edit_book, name='edit_book'),
-    path('book/delete/<int:pk>/', views.delete_book, name='delete_book'),
+        path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
 
     ]
