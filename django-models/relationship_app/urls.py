957efeaa65846566489014feab92cd4a5220c44d
sandysameh2 <sandy.sameh@nodogoro.com>
2024-09-01 13:33:59 +0300

    login+ logout adjust 2

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -6,9 +6,9 @@ from .views import LibraryDetailView,LoginView,LogoutView,register
 urlpatterns = [
     path('books/',list_books,name="list_books"),
     path('library/<int:pk>/',LibraryDetailView.as_view(),name='library_detail'),
-    path('login/',LoginView.as_view(),name="login"),
+    path('login/',LoginView.as_view(template_name="relationship_app/login.html"),name="login"),
 
-        path('logout/',LogoutView.as_view(),name="logout"),
+        path('logout/',LogoutView.as_view(template_name="relationship_app/logout.html"),name="logout"),
 
     path('register/',register,name="register"),
 
