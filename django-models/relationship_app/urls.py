333df8fd91605d610be3af3fa85ba1c6ebe03bda
sandysameh2 <sandy.sameh@nodogoro.com>
2024-09-01 16:31:38 +0300

    views

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
         path('logout/',LogoutView.as_view(template_name="relationship_app/logout.html"),name="logout"),
 
     path('register/',views.register,name="register"),
-    path('admin/',views.admin_view,name="admin_view"),
-    path('librarian/',views.librarian_view,name="librarian_view"),
-        path('member/', views.member_view, name='member_view'),
+    path('admin/',views.AdminView,name="admin_view"),
+    path('librarian/',views.LibrarianView,name="librarian_view"),
+        path('member/', views.MemberView, name='member_view'),
 
     
     ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -52,13 +52,13 @@ def is_member(user):
     return user.is_authenticated and user.profile.role == 'Librarian'
 
 @user_passes_test(is_admin)
-def admin_view(request):
+def AdminView(request):
     return HttpResponse("Welcome to the Admin view")
 
 @user_passes_test(is_librarian)
-def librarian_view(request):
+def LibrarianView(request):
     return HttpResponse("Welcome to the Librarian view")
 
 @user_passes_test(is_member)
-def member_view(request):
+def MemberView(request):
     return HttpResponse("Welcome to the Member view")
